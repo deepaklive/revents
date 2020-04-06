8438a47afad3cc10a28bf25b138743c44cd26426
unknown <deepak.verma@live.in>
2020-04-06 23:20:58 +0530

    End of Section 3

---
+++ src/app/features/event/EventList/EventList.jsx
@@ -0,0 +1,17 @@
+import React, { Component , Fragment } from 'react'
+import EventListitem from './EventListItem'
+
+class EventList extends Component {
+     render() {
+          return (
+               
+               <Fragment>
+                    {this.props.events.map(event => (
+                         <EventListitem key={event.id} event ={event}/>
+                    ))}
+               </Fragment>
+          )
+     }
+}
+
+export default EventList;
